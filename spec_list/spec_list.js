f2a3c9277282a592526eea162f6cdbee47c36126
AzuMap <histandard@ivory.plala.or.jp>
2017-03-12 20:56:37 +0900

    Add files via upload

--- spec_list/spec_list.js
+++ spec_list/spec_list.js
@@ -1,43 +1,42 @@
 "use strict";
+var _ = console.log;
 
-(function () {
-
-   var toggles = document.querySelectorAll(".toggle");
-   var i = 0;
-   
-   var hideshow = function (ev) {
-      
-      // ev.stopPropagation();
-      if (ev.target.className === "toggle") {
-      
-         var target = ev.target;
-         var n = 0;
-         
-         for(;n < target.children.length; n++) {
-            
-            var ul2 = target.children[n];
-         
-            if (ul2.style.display !== "none") {
-            
-               ul2.style.display = "none";
-            
+document.body.onload = function() {
+    var toggles = document.querySelectorAll(".toggle"),
+        i = 0,
+        lists = document.querySelectorAll('.lists');
+    
+    /*********************
+     if creating object property in 'for loop' , pre-defining object by literal is important! 
+    ************************/
+    for (;i < lists.length; i++) {
+        var list, listH, btn;
+        list = lists[i];
+        /************************************
+          overFlow => getRect() => height:0
+          overflow setting affects element height.
+        **************************************/
+        list.style.overflowY = 'hidden';
+        listH = list.getBoundingClientRect().height;
+        
+        list.style.height = '0';
+        
+        btn = list.previousElementSibling;
+        btn.height = listH; // list-Height assigns the button Object Property
+        btn.addEventListener('mousedown', function(ev) {
+            var elmH;
+            ev.preventDefault();
+            // _(this.height);
+            elmH = this.nextElementSibling.getBoundingClientRect().height;
+            // _(this.nextElementSibling.tagName, elmH);
+            if(elmH === 0) {
+                this.classList.add('clicked');
+                this.nextElementSibling.style.height = this.height + 'px';
             } else {
-         
-               ul2.style.display = "block";
-            
+                this.classList.remove('clicked');
+                this.nextElementSibling.style.height = 0;  
             }
-         
-         }
-      
-      }
-      
-   };
-   
-   for (;i < toggles.length; i++) {
-      
-      var toggle = toggles[i];
-      toggle.addEventListener("click", hideshow);
-      
-   }
-
-})();
+        });
+    }
+       // _(obj);
+};
